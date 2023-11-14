354597b620ea2fe551cc99f01ff8c404d5213ea4
IMBor1 <ilacerber1@gmail.com>
2023-11-14 22:31:40 +0300

    init

---
+++ src/Employee.java
@@ -0,0 +1,48 @@
+public class Employee {
+    static int count;
+    private String name;
+    private int departament;
+    private double salary;
+    int id;
+
+    public Employee(String name, int departament, double salary) {
+        this.name = name;
+        this.departament = departament;
+        this.salary = salary;
+        this.id = count;
+        count++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartament() {
+        return departament;
+    }
+
+    public void setDepartament(int departament) {
+        this.departament = departament;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + " ," +
+                " departament = " + departament +
+                ", salary=" + salary +
+                ", id=" + id;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,103 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] employee = new Employee[10];
+        employee[0] = new Employee("vasya", 1, 40000);
+        employee[1] = new Employee("petya", 3, 65000);
+        employee[2] = new Employee("artem", 5, 80000);
+        employee[3] = new Employee("sasha", 2, 50000);
+        employee[4] = new Employee("ilya", 4, 60000);
+        employee[5] = new Employee("gosha", 3, 72000);
+        employee[6] = new Employee("oleg", 2, 68000);
+        employee[7] = new Employee("kolya", 5, 78000);
+        employee[8] = new Employee("pasha", 3, 64000);
+        employee[9] = new Employee("vanya", 1, 570000);
+        minDepartment(employee, 5);
+    }
+
+    public static void listAll(Employee[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void salary(Employee[] arr) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i].getSalary();
+        }
+        System.out.println(sum);
+    }
+
+    public static void minSalary(Employee[] arr) {
+        double min = 1000000;
+        for (int i = 0; i < arr.length; i++) {
+            min = arr[i].getSalary();
+            if (arr[i].getSalary() > arr[i + 1].getSalary()) {
+                min = arr[i + 1].getSalary();
+            }
+        }
+        System.out.println(min);
+    }
+
+    public static void maxSalary(Employee[] arr) {
+        double max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int temp;
+            max = arr[i].getSalary();
+            if (arr[i].getSalary() < arr[i + 1].getSalary()) {
+                max = arr[i + 1].getSalary();
+            }
+        }
+        System.out.println(max);
+    }
+
+    public static void middleSalary(Employee[] arr) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i].getSalary();
+        }
+        System.out.println(sum / arr.length);
+    }
+
+    public static void allNames(Employee[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i].getName());
+        }
+    }
+
+    public static void salaryIndexing(Employee[] arr, double index) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i].setSalary(arr[i].getSalary() * index);
+
+        }
+    }
+
+    public static double minDepartment(Employee[] arr, int department) {
+        double min = 10000000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].getDepartament() == department && min > arr[i].getSalary()) {
+                min = arr[i].getSalary();
+                if (min > arr[i + 1].getSalary()) {
+                    min = arr[i + 1].getSalary();
+                }
+            }
+
+        }
+        return min;
+    }
+
+    public static double maxDepartment(Employee[] arr, int department) {
+        double max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].getDepartament() == department && max > arr[i].getSalary()) {
+                max = arr[i].getSalary();
+                if (max > arr[i + 1].getSalary()) {
+                    max = arr[i + 1].getSalary();
+                }
+            }
+
+        }
+        return max;
     }
 }
